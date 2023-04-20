75f91914e29e1688fbc75aa59d93b80b15534e81
bernsrepublic <bernsrepublic@gmail.com>
2023-04-19 20:34:35 -0700

    correction on index.js code block

--- index.js
+++ index.js
@@ -21,34 +21,8 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 app.use("/places", require("./controllers/places"));
 
-// GET /places
-app.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "http://placekitten.com/250/250",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "http://placekitten.com/250/250",
-    },
-  ];
-
-  res.render("places/index", { places });
-});
-
 app.get("*", (req, res) => {
   res.render("error404");
-});
-
-app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
 });
 
 app.listen(process.env.PORT);
