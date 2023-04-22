1c090fbdec345a7a9467a3eed42ff6dc96cf4f6a
bernsrepublic <bernsrepublic@gmail.com>
2023-04-21 21:03:40 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,13 @@ function error404() {
     <Def>
       <main>
         <h1>404: PAGE NOT FOUND</h1>
+        <div>
+          <img src="../images/milk-n-fruits.jpg" alt="Milk & Fruits" />
+          <div>
+            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on{" "}
+            <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
         <p>Oops, sorry, we can't find this page!</p>
       </main>
     </Def>
