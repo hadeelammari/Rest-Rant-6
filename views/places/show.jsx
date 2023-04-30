cf355cd4819cddbc0ace8e6f5ec0c2f14adfe07b
bernsrepublic <bernsrepublic@gmail.com>
2023-04-29 17:29:08 -0700

    places controller

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,41 +1,20 @@
 const React = require("react");
 const Def = require("../default");
 
-function Show(data) {
+function new_form(data) {
+  let message = "";
+  if (data.message) {
+    message = <h4 className="alert-danger">{data.message}</h4>;
+  }
   return (
     <Def>
-      <div className="container">
-        <h1>{data.place.name}</h1>
-        <p>{data.place.description}</p>
-
-        {/* {place.comments.length > 0 ? (
-          <ul>
-            {place.comments.map((comment, index) => (
-              <li key={index}>{comment}</li>
-            ))}
-          </ul>
-        ) : (
-          <h3>No comments yet!</h3>
-        )} */}
-
-        {data.place.rating ? (
-          <h3>Rating: {data.place.rating}</h3>
-        ) : (
-          <h3>Not rated yet!</h3>
-        )}
-
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-      </div>
+      <main>
+        <h1>Add a New Place</h1>
+        {message}
+        ...
+      </main>
     </Def>
   );
 }
 
-module.exports = Show;
+module.exports = new_form;
