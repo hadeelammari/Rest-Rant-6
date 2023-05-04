8032aa6c3ea06c9626f0faac8a583b1f6f62d085
bernsrepublic <bernsrepublic@gmail.com>
2023-05-04 14:41:10 -0700

    place schema update

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,26 +1,25 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show(place) {
+function show(data) {
   return (
     <Def>
       <main>
-        <h1>{place.name}</h1>
+        <h1>{data.place.name}</h1>
+        <section>currently unrated</section>
+        <section>Comments</section>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
       </main>
-
-      <a href={`/places/${place.id}/edit`} className="btn btn-warning">
-        Edit
-      </a>
-
-      <form method="POST" action={`/places/${place.id}?_method=DELETE`}>
-        <button type="submit" className="btn btn-danger">
-          Delete
-        </button>
-      </form>
-
-
     </Def>
-  )
+  );
 }
 
-module.exports = show
+module.exports = show;
