5a163ddb71abc54544cc41476d1412fc7e6bf00b
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-05-10 11:08:41 -0700

    Update show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -19,19 +19,37 @@ function show(data) {
   return (
     <Def>
       <main>
-        <div className="row">
-          ...
-          <div className="col-sm-6">
-            <h1>{data.place.name}</h1>
-            <h2>Rating</h2>
-            {rating}
-            <br />
-            ...
-          </div>
+        <h1>{data.place.name}</h1>
+        <img src={data.place.pic} alt={data.place.name} width="400px" />
+        <div id='description'>
+          <h2>Description</h2>
+          <h3>Located in {data.place.city}, {data.place.state}</h3>
         </div>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        <div id='comments'>
+          <h2>Comments</h2>
+          {comments}
+        </div>
+        <div id='rating'>
+          {rating}
+        </div>
+
+        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
       </main>
     </Def>
-  );
+  )
 
 }
 module.exports = show;
